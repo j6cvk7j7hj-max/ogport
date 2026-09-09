9908d3874b96ccc7d088c83221390ef28d1468f0
j6cvk7j7hj-max <j6cvk7j7hj@privaterelay.appleid.com>
2026-09-09 18:33:08 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/ogport',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
